4dc375aa95e2b42c792ffdeda99056502fcf45f8
Yigitcan Cakar <yigitcanckr97@gmail.com>
2021-01-23 16:25:45 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ValueAndReferanceTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //int,decimal,float,bool,double --------Value Type(Deger Tip)
+
+            //Array,Class,Interface -------Referance Type
+
+
+            int Num1 = 10;
+            int Num2 = 30;
+            Num1 = Num2;
+            Num2 = 65;
+
+            Console.WriteLine(Num1);
+
+
+            int[] Numbers = new int[] { 10, 20, 30 };
+            int[] Numbers2 = new int[] { 100, 200, 300 };
+            Numbers = Numbers2;
+            Numbers2[0] = 999;
+
+            Console.WriteLine(Numbers[0]);
+
+
+
+
+        }
+    }
+}
